09026e1586c6fac9abbd55fec2bf75f26a6820d9
Vy0618 <viniciusazevedo1a@gmail.com>
2025-08-11 14:17:33 -0300

    Create classPessoa.java

---
+++ Java/Segundo Semestre/Classes e Objetos/classPessoa.java
@@ -0,0 +1,38 @@
+public class classPessoa {
+String Nome;
+int Idade;
+String Endereco;
+String Profissao;
+
+        public String getNome() {
+    return Nome;
+        }
+    public void setNome(String Nome){
+        this.Nome = Nome;
+    }
+
+
+        public int getIdade(){
+    return Idade;
+    }
+        public void setIdade(int Idade){
+            this.Idade = Idade;
+        }
+
+
+        public String getEndereco(){
+            return Endereco;
+        }
+
+        public void setEndereco(String Endereco){
+            this.Endereco = Endereco;
+        }
+        public String getProfissao(){
+            return Profissao;
+        }
+        public void setProfissao(String Profissao){
+            this.Profissao = Profissao;
+        }
+
+
+    }
