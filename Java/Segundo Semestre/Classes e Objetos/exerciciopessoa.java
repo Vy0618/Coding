02ff5d8e9ba6db64d9d904d81922dd215dce71f5
Vy0618 <viniciusazevedo1a@gmail.com>
2025-08-12 22:20:18 -0300

    Update and rename objetctPessoa.java to exerciciopessoa.java

--- Java/Segundo Semestre/Classes e Objetos/exerciciopessoa.java
+++ Java/Segundo Semestre/Classes e Objetos/exerciciopessoa.java
@@ -1,5 +1,44 @@
-public class objectPessoa {
+public class exerciciopessoa {
     public static void main(String[] args) {
+
+        class classPessoa {
+            String Nome;
+            int Idade;
+            String Endereco;
+            String Profissao;
+
+            public String getNome() {
+                return Nome;
+            }
+            public void setNome(String Nome){
+                this.Nome = Nome;
+            }
+
+
+            public int getIdade(){
+                return Idade;
+            }
+            public void setIdade(int Idade){
+                this.Idade = Idade;
+            }
+
+
+            public String getEndereco(){
+                return Endereco;
+            }
+
+            public void setEndereco(String Endereco){
+                this.Endereco = Endereco;
+            }
+            public String getProfissao(){
+                return Profissao;
+            }
+            public void setProfissao(String Profissao){
+                this.Profissao = Profissao;
+            }
+
+
+        }
         classPessoa pessoa1 = new classPessoa();
         classPessoa pessoa2 = new classPessoa();
 
