5dabdb2b572a70d2f657e4740b0de21a218c799d
Vy0618 <viniciusazevedo1a@gmail.com>
2025-08-27 10:10:49 -0300

    Atividade Construtores

---
+++ OOP/constructMethod/ConstrutorPessoa.java
@@ -0,0 +1,13 @@
+package constructMethod;
+
+public class ConstrutorPessoa{
+    public static void main (String[]args){
+
+
+        Pessoa jogador = new Pessoa("Mohhamed Ali", 25);
+
+        System.out.println(jogador.getIdade());
+        System.out.println(jogador.getNome());
+    }
+}
+
