857819168e1b8e4c1fb43fbc16edb3aa3de8390b
Vy0618 <viniciusazevedo1a@gmail.com>
2025-10-01 15:43:09 -0300

    Add files via upload

---
+++ Spring Boot/calculadora/front_calculadora/Javascript/app.js
@@ -0,0 +1,44 @@
+
+
+document.getElementById('calcForm').addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const n1 = parseFloat(document.getElementById('n1').value);
+      const n2 = parseFloat(document.getElementById('n2').value);
+      const operacao = document.getElementById('operacao').value;
+    
+let resultado = 0;
+let erro = 0;
+
+switch(operacao) {
+			case "somar" : resultado = n1 + n2; break;
+			case "subtrair" : resultado = n1 - n2; break;
+			case "multiplicar" : resultado = n1 * n2; break;
+			case "dividir" :
+				 if (n2 == 0) {
+				erro="Não é possível dividir por zero";
+			}
+				else {
+					resultado = n1 / n2; break;
+			}
+			default: erro = "operação inválida";
+			
+			}
+
+	
+
+			document.getElementById('resultado').textContent = resultado;
+			document.getElementById('erro').textContent = erro;
+
+
+
+			
+if (erro==0){
+	document.getElementById("resultado").textContent= resultado;
+
+} else{
+	document.getElementById("erro").textContent= erro;
+}
+
+
+
+});
