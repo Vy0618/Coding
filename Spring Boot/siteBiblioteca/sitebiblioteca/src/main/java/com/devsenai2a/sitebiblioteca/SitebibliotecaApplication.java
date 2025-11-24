59828b0c06fdb3dbc5090df06eeddeab6cec19a2
Vy0618 <viniciusazevedo1a@gmail.com>
2025-11-24 15:57:52 -0300

    Add files via upload

---
+++ Spring Boot/siteBiblioteca/sitebiblioteca/src/main/java/com/devsenai2a/sitebiblioteca/SitebibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.devsenai2a.sitebiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitebibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitebibliotecaApplication.class, args);
+	}
+
+}
